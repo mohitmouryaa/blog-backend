d1195a01e6bf4aa66417211e19fdfe7194778d86
Aman <156819993+amanrawat98@users.noreply.github.com>
2025-01-02 17:31:15 +0530

    add function to verify JWT token

--- src/utility/generateJwtToken.ts
+++ src/utility/generateJwtToken.ts
@@ -1,4 +1,5 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
 import { jwtTokenData } from "../schemas/jwtSchema";
 
 export const generateJwtToken = (data: jwtTokenData): JwtPayload | string => {
@@ -7,4 +8,21 @@ export const generateJwtToken = (data: jwtTokenData): JwtPayload | string => {
   }
   let token = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: "12h" });
   return token;
+};
+
+export const verifyJwtToken = (req: Request, res: Response, next: NextFunction): void => {
+  const token = req.headers["authorization"]?.split(" ")[1];
+  if (!token) {
+    res.status(401).json({ message: "Unauthorized: No token provided" });
+    return;
+  }
+  if (!token) {
+    res.status(401).json({ message: "Unauthorized: Invalid token format" });
+    return;
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = decoded as jwtTokenData;
+
+  next();
 };
