4182db2677668dc826ad8647fda731bce5ffd466
Aman <156819993+amanrawat98@users.noreply.github.com>
2025-01-09 11:45:28 +0530

    Update route to add checkAuthor middleware for author validation

--- src/routes/blogRouter.ts
+++ src/routes/blogRouter.ts
@@ -1,12 +1,14 @@
 
 
 import express from "express";
-import { createBlog } from "../controllers/blog.controller";
+import { createBlog, getAllBlogs, getBlogById } from "../controllers/blog.controller";
 import { verifyJwtToken } from "../utility/generateJwtToken";
 import { checkAuthor } from "../middleware/checkRole";
 
 const router = express.Router();
 
-router.post('/blog',verifyJwtToken,  createBlog);
+router.post('/blog',verifyJwtToken, checkAuthor, createBlog);
+router.get('/blog',verifyJwtToken, checkAuthor, getAllBlogs);
+router.get('/blog/:id',verifyJwtToken, checkAuthor, getBlogById);
 
 export default router;
