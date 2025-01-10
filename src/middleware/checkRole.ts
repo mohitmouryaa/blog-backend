f7a32fe53c181cd0e52de4085bdc3d154d16f44d
Aman <156819993+amanrawat98@users.noreply.github.com>
2025-01-10 17:04:02 +0530

    Implement authorizeRole middleware to check user role for authorization

--- src/middleware/checkRole.ts
+++ src/middleware/checkRole.ts
@@ -1,17 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 
-export const checkAdmin = (req: Request, res: Response, next: NextFunction) => {
-  if (!req.user || req.user.role !== "admin") {
-     res.status(400).json({ message: "Only admin can access this route" });
-     return;
-  }
-  next();
-};
-
-export const checkAuthor = (req: Request, res: Response, next: NextFunction) => {
-  if (!req.user || req.user.role !== "author") {
-    res.status(400).json({ message: "Only author can access this route" });
-    return;
-  }
-  next();
+export const authorizeRole = (role:string) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user || req?.user?.role !== role) {
+      res.status(400).json({ message: `Only ${role} can access this route` });
+      return;
+    }
+    next();
+  };
 };
