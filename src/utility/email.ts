d48fcada140adfa158dcfe6915c93b1e47c3a1b4
Aman <156819993+amanrawat98@users.noreply.github.com>
2025-01-10 11:02:03 +0530

    Create sendEmail utility function to send email using transporter.sendMail

--- src/utility/email.ts
+++ src/utility/email.ts
@@ -9,3 +9,18 @@ const transporter = nodemailer.createTransport({
     pass:process.env.EMAIL_PASS
    }
 })
+
+export const sendEmail = async (to: string, subject: string, text: string) => {
+    
+    try {
+      await transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to,
+        subject,
+        text,
+      });
+    } catch (error) {
+      console.error('Error sending email:', error);
+      throw new Error('Email notification failed');
+    }
+  };
