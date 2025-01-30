28d9dc84bf163618f33b8c9fd0a048c2050f32bb
Aman <156819993+amanrawat98@users.noreply.github.com>
2025-01-30 10:16:00 +0530

    Add upload.single middleware to create blog router

--- src/routes/blogRouter.ts
+++ src/routes/blogRouter.ts
@@ -2,10 +2,11 @@ import express from "express";
 import { createBlog, getAllBlogs, getBlogById, updateBlogStatus } from "../controllers/blog.controller";
 import { verifyJwtToken } from "../utility/generateJwtToken";
 import { authorizeRole } from "../middleware/checkRole";
+import upload from "../config/multerConfig";
 
 const router = express.Router();
 
-router.post("/blog", verifyJwtToken, authorizeRole("author"), createBlog);
+router.post("/blog", verifyJwtToken, authorizeRole("author"), upload.single('image'),createBlog);
 router.get("/blog", getAllBlogs);
 router.get("/blog/:id", getBlogById);
 router.put("/blog/:id/:status", verifyJwtToken, authorizeRole("admin"), updateBlogStatus);
