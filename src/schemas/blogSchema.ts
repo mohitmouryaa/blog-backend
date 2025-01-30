29055c6c1a741e6ad63972f64cadc5d5aa1a0072
Aman <156819993+amanrawat98@users.noreply.github.com>
2025-01-30 10:14:26 +0530

    Update createBlogSchema to handle multipart form data

--- src/schemas/blogSchema.ts
+++ src/schemas/blogSchema.ts
@@ -1,8 +1,15 @@
 import {z} from "zod";
 
 export const createBlogSchema = z.object({
-    title:z.string().min(4,"title should be min 4 characters"),
-    content:z.string()
+  title: z.string().min(4, "Title should be at least 4 characters"),
+  content: z.string(),
+  thumbnail: z.object({
+    buffer: z.instanceof(Buffer), // Make sure it's a Buffer
+    mimetype: z.string().refine((mimetype) => mimetype.startsWith("image/"), {
+      message: "Invalid file type. Only images are allowed.",
+    }),
+    size: z.number().max(5 * 1024 * 1024, "File size should not exceed 5MB"), // Max size 5MB
+  }),
 }).strict()
 
 export const blogStatusSchema = z.enum(["approved", "rejected"]);
